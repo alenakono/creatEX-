42722363790c92dddab8209f0b48d4e420592258
Alena <starsingrass@gmail.com>
2022-09-07 16:09:06 +0300

    feat: fix popup js

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,17 @@
+//mobile header menu
 $(document).ready(function () {
-    $(".header__menu_mob").on("click", function (event) {
-        $(".header__menu_mob, .header-nav_mob").toggleClass("_active");
-        $("body").toggleClass("_lock");
-    });
+  $(".header__menu_mob").on("click", function (event) {
+    $(".header__menu_mob, .header-nav_mob").toggleClass("_active");
+    $("body").toggleClass("_lock");
+  });
 });
 const swiperTeam = new Swiper('.team-swiper', {
-    loop: true,
-    navigation: {
-      nextEl: '.team-swiper__next',
-      prevEl: '.team-swiper__prev',
-    },
-    slidesPerView: 4,
+  loop: true,
+  navigation: {
+    nextEl: '.team-swiper__next',
+    prevEl: '.team-swiper__prev',
+  },
+  slidesPerView: 4,
   spaceBetween: 30,
   breakpoints: {
     1230: {
@@ -25,22 +26,24 @@ const swiperTeam = new Swiper('.team-swiper', {
   }
 });
 const swiperReview = new Swiper('.review-swiper', {
-    loop: true,
-    slidesPerView: 1,
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    navigation: {
-      nextEl: '.review-swiper__next',
-      prevEl: '.review-swiper__prev',
-    },
-  
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.review-swiper__next',
+    prevEl: '.review-swiper__prev',
+  },
+
 });
+//footer dropdown
 $(document).ready(function () {
-  $(".footer-dropdown__title").on("click", function (event){
-      $(this.nextElementSibling).toggleClass("_active");
+  $(".footer-dropdown__title").on("click", function (event) {
+    $(this.nextElementSibling).toggleClass("_active");
   });
 });
+//eventsLectures view
 $(document).ready(function () {
   $(".eventsLectures-nav__type_tile").on("click", function (event) {
     $(".eventsLectures-nav__type").removeClass("_active");
@@ -53,3 +56,34 @@ $(document).ready(function () {
     $(".eventsLectures-block").removeClass("_tile");
   });
 });
+//popup
+const openPopupLogin = document.querySelectorAll('.popup-login__open');
+const openPopupRegister = document.querySelector('.popup-register__open');
+const closePopup = document.querySelectorAll('.popup__close');
+const popupLogin = document.getElementById('popup-login');
+const popupRegister = document.getElementById('popup-register');
+const popups = document.querySelectorAll('.popup');
+
+if (popups) {
+  openPopupLogin.forEach(button => {
+    button.addEventListener('click', () => {
+      popupLogin.classList.add('_active');
+    });
+  });
+  openPopupRegister.addEventListener('click', () => {
+    popupRegister.classList.add('_active');
+  });
+  closePopup.forEach(close => {
+    close.addEventListener('click', () => {
+      popups.forEach(popup => {
+        popup.classList.remove('_active');
+      });
+    });
+  });
+}
+if (popupRegister) {
+  const registerClose = popupRegister.querySelector('.popup-login__open');
+  registerClose.addEventListener('click', () => {
+    popupRegister.classList.remove('_active');
+  });
+}
